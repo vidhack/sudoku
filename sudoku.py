0f86b75989921b30c7d4c0198c3bb11f98546ad5
Vidya Sagar <sagarv@vmware.com>
2017-04-01 22:30:03 +0530

    initialize row, col and grid

--- sudoku.py
+++ sudoku.py
@@ -13,6 +13,7 @@ class Sudoku:
          for each_row in self.row_string for each_col in self.col_string]
         self._init_row()
         self._init_col()
+        self._init_grid()
 
     def _init_row(self):
         self.row = defaultdict(dict)
@@ -29,12 +30,18 @@ class Sudoku:
                                    each_row] = getattr(self, each_col + each_row)
 
     def _init_grid(self):
-        self.grid = defaultdict(tuple)
-        split_string = lambda x, n: [x[i:i+n] for i in range(0, len(x), n)]
+        self.grid = defaultdict(set)
+        split_string = lambda x, n: [x[i:i + n] for i in range(0, len(x), n)]
         col_group = split_string(self.col_string, 3)
         row_group = split_string(self.row_string, 3)
-        for each_col_group in col_group:
-
+        for each_grid in range(1, 10):
+            row_group_no = (each_grid - 1) / 3
+            col_group_no = (each_grid - 1) % 3
+            grid_col = col_group[col_group_no]
+            grid_row = row_group[row_group_no]
+            grid_cell = set(["".join(each_cell)
+                             for each_cell in itertools.product(grid_col, grid_row)])
+            self.grid[each_grid] = grid_cell
 
     def draw_sudoku(self):
         for each_col in self.col_string:
@@ -42,7 +49,8 @@ class Sudoku:
         print '\n'
         for each_row in self.row:
             for each_col in self.col:
-                print getattr(self, each_col + each_row),
+                cell_options =  getattr(self, each_col + each_row)
+                print "".join([str(each) for each in cell_options]),
             print '\n'
 
 
